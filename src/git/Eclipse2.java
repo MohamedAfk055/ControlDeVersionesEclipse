e870587de68496996c7a203581a62866ead10c02
achou <machouragh@iesgpb.net>
2024-04-04 20:22:23 +0200

    Version1.0

---
+++ src/git/Eclipse2.java
@@ -0,0 +1,14 @@
+package git;
+import java.util.Scanner;
+public class Eclipse2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private static double calcularRaizCuadrada(double a) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
